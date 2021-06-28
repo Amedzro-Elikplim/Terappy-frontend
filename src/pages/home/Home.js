bb9b59326567448791fdde87ea305a8f93a33e1e
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2021-06-28 21:19:54 +0000

    added booking section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,12 +4,14 @@ import Footer from "../../components/HomePageSections/Footer";
 import Services from "../../components/HomePageSections/Services";
 import AboutUs from "../../components/HomePageSections/AboutUs";
 import Products from "../../components/HomePageSections/Products";
+import Booking from "../../components/HomePageSections/Booking";
 const Home = () => {
     return(
         <div className="container">
              <Nav />
              <Hero />
              <Services />
+             <Booking />
              <Products />
              <AboutUs />
              <Footer />
